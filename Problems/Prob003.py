1b5a098291ef8a9ecf9b3b4948839a10246016f4
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-08-24 22:01:14 -0700

    feat: Set up LeetCode problems 1-5 for practice

---
+++ Problems/Prob003.py
@@ -0,0 +1,14 @@
+# Given a string s, find the length of the longest substring without repeating characters.
+
+# Example
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
---
+++ Problems/Prob005.py
@@ -0,0 +1,14 @@
+# Given a string s, return the longest palindromic substring in s.
+
+# Example
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
